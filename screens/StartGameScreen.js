a1994ebc30d85f6129670c48b6cdf68511ac3df5
Spearek <30389214+Spearek@users.noreply.github.com>
2020-03-05 14:12:29 +0100

    screate StartGameScreen component

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { View, StyleSheet, Text, TextInput, Button } from 'react-native';
+
+import Card from '../components/Card';
+import Colors from '../constants/colors';
+
+const startGameScreen = props => {
+
+    return (
+        <View style={styles.screen}>
+            <Text style={styles.title}>Start na New Game!</Text>
+            <Card style={styles.inputContainer}>
+                <Text >Select a Number</Text>
+                <TextInput />
+                <View style={styles.buttonsContainer}>
+                    <View style={styles.button}><Button title="Reset" onPress={() => { }} color={Colors.accent} /></View>
+                    <View style={styles.button}><Button title="Confirm" onPress={() => { }} color={Colors.primary} /></View>
+                </View>
+            </Card>
+        </View>
+    )
+
+}
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 10,
+        alignItems: 'center'
+    },
+    title: {
+        fontSize: 20,
+        marginVertical: 10,
+    },
+    inputContainer: {
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center'
+    },
+
+    buttonsContainer: {
+        flexDirection: "row",
+        width: '100%',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15
+    },
+    button: {
+        width: '40%'
+    }
+});
+
+export default startGameScreen;
