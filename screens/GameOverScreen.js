479c7e7b6b4de928c3f5936b11e116aff2fb2eae
Spearek <30389214+Spearek@users.noreply.github.com>
2020-03-23 12:28:56 +0100

    project image added into GameOverScreen component

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, Image } from 'react-native';
+
+import BodyText from '../components/BodyText';
 
 const GameOverScreen = props => {
 
     return (
         <View style={styles.screen}>
-            <Text>The Game is Oven hahaha, you get it? OVEN! ;DDD</Text>
-            <Text>Number of rounds: {props.roundsNumber}</Text>
-            <Text>Number was: {props.userNumber}</Text>
-            <Button title="New Game" onPress={props.onRestart}/>
+            <BodyText>The Game is Oven hahaha, you get it? OVEN! ;DDD</BodyText>
+            <View style={styles.imageContainer}>
+                <Image
+                    //fadeDuration={300}
+                    // source={require('../assets/success.png')}
+                    source={{uri: 'https://images.unsplash.com/photo-1554629947-334ff61d85dc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=676&q=80'}}
+                    style={styles.image}
+                    resizeMode='cover'
+                />
+            </View>
+
+            <BodyText>Number of rounds: {props.roundsNumber}</BodyText>
+            <BodyText>Number was: {props.userNumber}</BodyText>
+            <Button title="New Game" onPress={props.onRestart} />
         </View>
     );
 };
@@ -18,6 +30,19 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    imageContainer: {
+        width: 300,
+        height: 300,
+        borderRadius: 150,
+        borderWidth: 3,
+        borderBottomColor: 'black',
+        overflow: 'hidden',
+        marginVertical: 30
+    },
+    image: {
+        width: '100%',
+        height: '100%'
     }
 
 })
